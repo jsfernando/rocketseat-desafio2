48b954fccc177940b08550b7709f3ded003538d0
fernandojsilvasenac <fernando.jsilva.senac@gmail.com>
2022-05-20 14:56:01 -0300

    eleveçado do estado do app para SideBar e Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,62 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+import { MovieCard } from './MovieCard';
+
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
 }
+
+//2ª
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+// 1ª
+interface ContentProps{
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+}
+
+//                              1ª             2ª
+export function Content( { selectedGenreId, selectedGenre } : ContentProps ) {
+  // Complete aqui
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+  }, [selectedGenreId]);
+
+  return(
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  
+  )
+
+}
+
+// o estado compartilhado do App entre a SideBar e Content é o selectedGenreId que está sendo recebido por props
+// sendo passado pelo App
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,54 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from 'react';
+import { Button } from './Button';
+
+import { api } from '../services/api';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
+
+// 4ª
+interface SideBarProps{
+  handleClickButton: (id: number) => void; // porque o retorno é void ? pq. na chamada do click não existe o return
+  selectedGenreId: number;
+}
+
+
+//                            3ª                  4ª
+export function SideBar({handleClickButton, selectedGenreId}: SideBarProps) {
+  // Complete aqui
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+
+
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+    </nav>
+  )
+
+}
+
+// o estado compartilhado do App entre a SideBar e Content é o selectedGenreId que está sendo recebido por props
+// sendo passado pelo App
